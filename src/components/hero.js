8dcfa703330997b1fed4bf2104aa97ba4f6a8371
Alec Diaz <adiaz@zynga.com>
2019-08-26 17:01:46 -0700

    centered hero content

--- src/components/hero.js
+++ src/components/hero.js
@@ -7,14 +7,16 @@ const TextBox = styled('div')`
   flex-direction: column;
   height: 60vh;
   justify-content: center;
+  align-items: center;
   /* padding: 0 calc((100vw - 550px) / 2) 2rem; */
-  width: 75%;
+  width: 100%;
   margin-bottom: 4rem;
 
   h1 {
     color: white;
     font-size: 2.25rem;
     font-weight: 800;
+    margin-bottom: 1rem;
   }
 
   p,
@@ -22,7 +24,7 @@ const TextBox = styled('div')`
     color: white;
     font-size: 1.5rem;
     font-weight: 300;
-    margin-top: 1rem;
+    /* margin-top: rem; */
   }
 
   a {
@@ -37,13 +39,12 @@ const TextBox = styled('div')`
   }
 
   .contact {
-    margin-left: 2rem;
-    margin-right: 2rem;
-    width: 6rem;
+    /* margin-right: 2rem; */
+    width: 10rem;
     display: flex;
     flex-direction: row;
     align-content: center;
-    justify-content: space-between;
+    justify-content: center;
     transition: .2s;
 
     &:hover {
@@ -51,30 +52,15 @@ const TextBox = styled('div')`
     }
   }
   
-  @media only screen and (max-width: 900px) {
-    margin-left: 2rem;
-
-    .contact  {
-      margin-left: 0;
-    }
-  }
-
-  @media only screen and (max-width: 430px) {
-    height: 60vh;
-    
-    .contact  {
-      margin-left: 0;
-    }
-  }
 `;
 
 const Hero = () => {
   return (
     <TextBox>
       <h1>Alec Diaz</h1>
-      <p>
-      Listen, strange women lyin' in ponds distributin' swords is no basis for a system of government. Supreme executive power derives from a mandate
-      </p>
+      <p>HCI student at UC Irvine</p>
+      <p>Research assistant at HAI</p>
+      <p>UX/UI intern at Zynga</p>
       <div class="contact-container">
         <a class="contact" href="#">
           <svg id="i-clipboard" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
